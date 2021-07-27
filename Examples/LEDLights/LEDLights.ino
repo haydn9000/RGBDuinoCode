9b8005206bdb07210210197e840ad6dfc9e45188
Haydn <31514694+haydnady@users.noreply.github.com>
2021-07-27 18:38:33 -0400

    Updated

--- Examples/LEDLights/LEDLights.ino
+++ Examples/LEDLights/LEDLights.ino
@@ -1,6 +1,5 @@
 int brightness = 15;
 
-
 void setup()
 {
     // Innitialize LED pins.     
@@ -19,13 +18,14 @@ void setup()
 // Some of these are not dimmable, fix this to reflect
 void loop()
 {
-    analogWrite(2, brightness);  // Digital pin
+    digitalWrite(2, HIGH);  // Digital pin
+    digitalWrite(4, HIGH);  // Digital pin
+    digitalWrite(7, HIGH);  // Digital pin
+    digitalWrite(8, HIGH);  // Digital pin
+    
     analogWrite(3, brightness);  // ~ Analog pin
-    analogWrite(4, brightness);  // Digital pin
     analogWrite(5, brightness);  // ~ Analog pin
     analogWrite(6, brightness);  // ~ Analog pin
-    analogWrite(7, brightness);  // Digital pin
-    analogWrite(8, brightness);  // Digital pin
     analogWrite(9, brightness);  // ~ Analog pin
     analogWrite(10, brightness);  // ~ Analog pin
     analogWrite(11, brightness);  // ~ Analog pin
